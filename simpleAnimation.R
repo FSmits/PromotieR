0b18a7ab927bd7d3288292a273ee2b65a3214717
XandraPlas <a.plas-2@umcutrecht.nl>
2023-03-16 16:29:16 +0100

    nieuwe data

--- simpleAnimation.R
+++ simpleAnimation.R
@@ -4,19 +4,19 @@ library(gifski)
 
 EC <- data.frame(
   year <- rep(rep((2000:2023), 5)),
-  group <- rep(c("Elbert", "senior's", "phd's", "research assistents", "interns"), each = 24)
+  group <- rep(c("Elbert", "seniors", "phd's", "research assistents", "interns"), each = 24)
 )
 colnames(EC) <- c("year", "group")
 
-EC$productivity[EC$group == "Elbert"]   <- sample(2:8, 24, replace = TRUE)
-EC$productivity[EC$group == "Seniors"]   <- sample(2:6, 24, replace = TRUE)
-EC$productivity[EC$group == "phd's"]   <- sample(6:9, 24, replace = TRUE)
-EC$productivity[EC$group == "research assistents"]   <- sample(1:10, 24, replace = TRUE)
-EC$productivity[EC$group == "interns"]   <- sample(3:7, 24, replace = TRUE)
+EC$productivity[EC$group == "Elbert"]   <- c(seq(from=1,to=4,by=0.5), seq(from=3,to=7,by=1), rep(c(5,6,7), 4)) 
+EC$productivity[EC$group == "seniors"]   <- c(seq(from=1,to=4,by=1), seq(from=7,to=3,by=-1), seq(from=2,to=7,by=0.5), seq(from=5,to=8,by=1))
+EC$productivity[EC$group == "phd's"]   <- seq(from=1,to=13,by=0.5)
+EC$productivity[EC$group == "research assistents"]   <- c(seq(from=1,to=8,by=1), rep(c(8,9), 8))
+EC$productivity[EC$group == "interns"]   <- c(seq(from=3,to=8,by=0.25), 7.5, 7, 6.5)
 
-
+#[EC$group!="phd's",]
 ## standard ggplot2
-myPlot <- ggplot(EC[EC$group!="phd's",], aes(x = group, y = productivity)) +
+myPlot <- ggplot(EC, aes(x = group, y = productivity)) +
   geom_bar(stat = "summary") +
   scale_colour_manual(values = country_colors) +
   # Here comes the gganimate specific bits
@@ -24,7 +24,7 @@ myPlot <- ggplot(EC[EC$group!="phd's",], aes(x = group, y = productivity)) +
   transition_time(year) +
   ease_aes('linear')
 
-animate(myPlot, duration = 5, fps = 20, renderer = gifski_renderer())
+animate(myPlot, duration = 15, fps = 5, renderer = gifski_renderer())
 anim_save("output.gif")
 
 
