e6a373c9d11c8b1c86eb1b0f32f29503148e0dd7
XandraPlas <a.plas-2@umcutrecht.nl>
2023-07-10 15:51:59 +0200

    kleine aanpassing funVED script

--- funVED_sport.R
+++ funVED_sport.R
@@ -24,12 +24,12 @@ oefeningen <- data.frame(full_body = c("Plank", "Burpees", "Push-Ups", "Jump Squ
 if (training_focus == "A") {
   if (training_niveau == "A") {
     writeLines(paste("Jij wil je in de funVED focussen op het hele lichaam.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "full_body"], "\n-", oefeningen[2, "full_body"], "\n-", oefeningen[3, "full_body"], "\n-", oefeningen[4, "full_body"], "\n-", oefeningen[5, "full_body"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een rustig niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 1.")
     
   }else if (training_niveau == "B"){
     writeLines(paste("Jij wil je in de funVED focussen op het hele lichaam.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "full_body"], "\n-", oefeningen[2, "full_body"], "\n-", oefeningen[3, "full_body"], "\n-", oefeningen[4, "full_body"], "\n-", oefeningen[5, "full_body"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het grote loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een gemiddeld niveau wissel \njij deze oefeningen af met het grote loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 2.")
     
   }else if (training_niveau == "C"){
@@ -42,12 +42,12 @@ if (training_focus == "A") {
 if (training_focus == "B") {
   if (training_niveau == "A") {
     writeLines(paste("Jij wil je in de funVED focussen op de armen.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "armen"], "\n-", oefeningen[2, "armen"], "\n-", oefeningen[3, "armen"], "\n-", oefeningen[4, "armen"], "\n-", oefeningen[5, "armen"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een rustig niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 1.")
     
   }else if (training_niveau == "B"){
     writeLines(paste("Jij wil je in de funVED focussen op de armen.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "armen"], "\n-", oefeningen[2, "armen"], "\n-", oefeningen[3, "armen"], "\n-", oefeningen[4, "armen"], "\n-", oefeningen[5, "armen"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het grote loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een gemiddeld niveau wissel \njij deze oefeningen af met het grote loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 2.")
     
   }else if (training_niveau == "C"){
@@ -60,12 +60,12 @@ if (training_focus == "B") {
 if (training_focus == "C") {
   if (training_niveau == "A") {
     writeLines(paste("Jij wil je in de funVED focussen op de benen.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "benen"], "\n-", oefeningen[2, "benen"], "\n-", oefeningen[3, "benen"], "\n-", oefeningen[4, "benen"], "\n-", oefeningen[5, "benen"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een rustig niveau wissel \njij deze oefeningen af met het kleine loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 1.")
     
   }else if (training_niveau == "B"){
     writeLines(paste("Jij wil je in de funVED focussen op de benen.\n\nDit zijn jouw oefeningen:\n-", oefeningen[1, "benen"], "\n-", oefeningen[2, "benen"], "\n-", oefeningen[3, "benen"], "\n-", oefeningen[4, "benen"], "\n-", oefeningen[5, "benen"]))
-    writeLines("\nOmdat jij wil sporten op een intensief niveau wissel \njij deze oefeningen af met het grote loop rondje.")
+    writeLines("\nOmdat jij wil sporten op een gemiddeld niveau wissel \njij deze oefeningen af met het grote loop rondje.")
     writeLines("Voor het laatste spel wordt jouw vermenigvuldigingsfactor 2.")
     
   }else if (training_niveau == "C"){
