a8563b46df0bec1a69b4167196e70fddfc5d5a92
FSmits <f.m.smits-2@umcutrecht.nl>
2024-12-12 13:28:13 +0100

    kleine aanpassing

--- Folder_size_VED.R
+++ Folder_size_VED.R
@@ -2,9 +2,10 @@ rm(list=ls())
 
 yourname <- "Fenne" ### type the name of your folder
 yourpath <- "/Volumes/heronderzoek-9/MGGZ/" ### type the path to MGGZ directory on O:-drive
-pathname <- paste("/Volumes/heronderzoek-9/MGGZ/", yourname, sep="")
+
+pathname <- paste(yourpath, yourname, sep="")
+
 setwd(pathname)
+
 totalsize <- sum(file.info(dir())$size) / 1000000
 totalsize #size in GB
-
-
